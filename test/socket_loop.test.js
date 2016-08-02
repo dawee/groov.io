083d33ef8abebb5ca19ecc329b4de4d0c26c18ff
David Corticchiato <david.corticchiato@gmail.com>
2016-08-02 10:57:18 +0200

    no need for context keeper, ref-struct seems to keep buffer references

--- test/socket_loop.test.js
+++ test/socket_loop.test.js
@@ -10,17 +10,17 @@ const net = require('net');
  * C Structs
  */
 
-const unio_text_t = Struct();
-unio_text_t.defineProperty('base', ref.types.CString);
-unio_text_t.defineProperty('len', ref.types.size_t);
+const uv_buf_t = Struct();
+uv_buf_t.defineProperty('base', ref.types.CString);
+uv_buf_t.defineProperty('len', ref.types.size_t);
 
 const unio_config_t = Struct();
-unio_config_t.defineProperty('host_address', ref.refType(unio_text_t));
+unio_config_t.defineProperty('host_address', ref.refType(uv_buf_t));
 unio_config_t.defineProperty('port', ref.types.int);
 
 const unio_event_t = Struct();
-unio_event_t.defineProperty('name', ref.refType(unio_text_t));
-unio_event_t.defineProperty('data', ref.refType(unio_text_t));
+unio_event_t.defineProperty('name', ref.refType(uv_buf_t));
+unio_event_t.defineProperty('data', ref.refType(uv_buf_t));
 
 const unio_event_stack_t = Struct();
 
@@ -31,10 +31,10 @@ unio_event_stack_t.defineProperty('events', ref.refType(unio_event_t));
  * Helpers
  */
 
-function createConfig(ctx, hostName, port) {
-  ctx.hostNameBuf = new Buffer(hostName);
-  ctx.hostName = new unio_text_t({base: ctx.hostNameBuf, len: ctx.hostNameBuf.length});
-  ctx.config = new unio_config_t({host_address: ctx.hostName.ref(), port: port}); 
+function createConfig(hostName, port) {
+  hostNameBuf = new Buffer(hostName);
+  hostName = new uv_buf_t({base: hostNameBuf, len: hostNameBuf.length});
+  return new unio_config_t({host_address: hostName.ref(), port: port}); 
 }
 
 
@@ -57,28 +57,24 @@ describe('socket loop', () => {
   });
 
   it('should connect to server', (done) => {
-    const ctx = {};
     const io = socketIO();
-
-    createConfig(ctx, "127.0.0.1", 3000);
+    const config = createConfig("127.0.0.1", 3000);
 
     const server = net.createServer(() => {
       server.close();
       done();
     }).listen(3000);
 
-    lib.unio_init(ctx.config.ref());
+    lib.unio_init(config.ref());
   });
 
   it('should read a connect event', (done) => {
-    const ctx = {};
     const io = socketIO();
-
-    createConfig(ctx, "127.0.0.1", 3000);
+    const config = createConfig("127.0.0.1", 3000);
 
     const server = net.createServer(() => {}).listen(3000);
 
-    lib.unio_init(ctx.config.ref());
+    lib.unio_init(config.ref());
     setInterval(() => {
      const count = lib.unio_read_incoming_events();
 
