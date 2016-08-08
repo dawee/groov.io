0b9bdddface5dd6cec9749ccb0e95c81d78c351e
David Corticchiato <david.corticchiato@gmail.com>
2016-08-08 10:17:56 +0200

    remove serializer

--- src/handshake.c
+++ src/handshake.c
@@ -1,5 +1,21 @@
 #include "groov.h"
 
+#include <stdio.h>
+#include <string.h>
+
+/*
+ * Request
+ */
+
+static char host_name[GROOV_MAX_HOST_NAME_SIZE];
+static int host_port;
+static const char * handshake_request_header = "GET /socket.io/?transport=websocket HTTP/1.1";
+static const char * handshake_request_connection = "Connection: Upgrade";
+static const char * handshake_request_upgrade = "Upgrade: websocket";
+
+/*
+ * Response
+ */
 
 static int received = 0;
 static char buf_base[GROOV_MAX_HANDSHAKE_SIZE];
@@ -20,6 +36,11 @@ static int groov_handshake__ended() {
     && groov_handshake__is_new_line_at(buf.len - 2);
 }
 
+void groov_init_handshake(groov_config_t * config) {
+  host_port = config->host_port;
+  memcpy(host_name, config->host_name->base, config->host_name->len);
+}
+
 void groov_reset_handshake_parser() {
   received = 0;
   buf.len = 0;
@@ -35,4 +56,19 @@ void groov_stream_to_handshake_parser(char byte) {
     received = 1;
     groov_write_incoming_handshake_event();
   }
+}
+
+void groov_serialize_handshake_request(groov_message_event_t * message) {
+  sprintf(message->base,
+    "%s\n%s\n%s\nHost: %s:%d\nSec-WebSocket-Version: %d\nSec-WebSocket-Key:%s\n\n",
+    handshake_request_header,
+    handshake_request_connection,
+    handshake_request_upgrade,
+    host_name,
+    host_port,
+    GROOV_PROTOCOL_VERSION,
+    "MTMtMTQ2OTcwMDczOTcwNA=="
+  );
+
+  message->len = strlen(message->base);
 }
